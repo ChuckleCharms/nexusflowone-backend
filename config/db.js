78ff25fca28713c86976f0ae3563187a3d24e9ea
ChuckleCharms <145775387+ChuckleCharms@users.noreply.github.com>
2025-12-10 12:05:39 -0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,19 @@
+// config/db.js
+import pkg from "pg";
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected PG client error", err);
+});
+
+export { pool };
 
